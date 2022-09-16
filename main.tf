2824b4d53bb2fdcbb453a3214d4329eb7e2e2f0c
Grant Orchard <go@hashicorp.com>
2022-09-16 15:06:43 +1000

    scale down networks

--- main.tf
+++ main.tf
@@ -1,15 +1,24 @@
 module networks {
   source  = "github.com/grantorchard/terraform-nsx-quickstart-module"
 
-  private_subnets = [
-    "10.0.3.0/28",
-    "10.0.3.16/28",
-    "10.0.3.32/28"
-  ]
-  public_subnets = [
-    "10.0.3.48/28",
-    "10.0.3.64/28",
-    "10.0.3.80/28"
-  ]
 	environment = var.environment
+  private_subnets = ["10.0.3.0/28"]
+  public_subnets = ["10.0.3.48/28"]
+
 }
+
+# module web {
+# 	source = "github.com/grantorchard/terraform-vsphere-module-virtual-machine"
+# 	count = 1
+# 	datacenter        = "Datacenter"
+#   cluster           = "Cluster"
+#   primary_datastore = "vsanDatastore"
+#   networks = {
+#     "seg-general" : "dhcp"
+#   }
+#   template = "go-tfc-agent-small"
+
+# 	hostname = "web-${count.index}"
+# 	memory = 4096
+
+# }
