2096074e34c5fa700b32446f649b51a0fdcb7680
Grant Orchard <go@hashicorp.com>
2022-09-16 17:44:23 +1000

    increase web count

--- main.tf
+++ main.tf
@@ -2,19 +2,27 @@ module networks {
   source  = "github.com/grantorchard/terraform-nsx-quickstart-module"
 
 	environment = var.environment
-  private_subnets = ["10.0.3.0/28"]
-  public_subnets = ["10.0.3.48/28"]
+  private_subnets = [
+    "10.0.3.0/28",
+    "10.0.3.16/28",
+    "10.0.3.32/28"
+  ]
+  public_subnets = [
+    "10.0.3.48/28",
+    "10.0.3.64/28",
+    "10.0.3.80/28"
+  ]
 
 }
 
 module web {
 	source = "github.com/terraform-vsphere-modules/terraform-vsphere-virtual-machine"
-	count = 1
+	count = 3
 	datacenter        = "Datacenter"
   cluster           = "Cluster"
   primary_datastore = "vsanDatastore"
   networks = {
-    "${module.networks.public_networks[0]}" : "dhcp"
+    "${module.networks.public_networks[count.index]}" : "dhcp"
   }
   template = "go-tfc-agent-small"
 
